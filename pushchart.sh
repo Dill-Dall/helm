3435c1b96374eaa0875321001a0fd263d9de77a7
DillDall <t.a.dahll@gmail.com>
2024-07-15 22:05:28 +0200

    Update Helm chart repository with valheim chart

---
+++ pushchart.sh
@@ -0,0 +1,17 @@
+# Ensure you are on the main branch and package the Helm chart
+git checkout main
+helm package .
+
+# Switch to the gh-pages branch and move the packaged chart
+git checkout gh-pages
+mv ../valheim-* .
+
+# Update the Helm repository index
+helm repo index . --url https://Dill-Dall.github.io/valhem-helm
+touch .nojekyll
+
+# Add, commit, and push the changes
+git add .
+git commit -m "Update Helm chart repository with valheim chart"
+git push origin gh-pages --force
+
